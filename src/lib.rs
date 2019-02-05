5d6328bfaf1f586467bd080cd3a7b8247ed5348a
Rob Saunders <rob@wearebrandnew.com>
2019-02-05 10:11:17 +0800

    further rust2018 changes

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,16 @@ pub mod config;
 pub mod error;
 pub mod exchanges;
 pub mod models;
-pub mod presenters;
 pub mod socket;
-pub mod indicators;
 pub mod threadpool;
+pub mod presenters {
+    mod asset;
+    mod funds;
+    mod position;
+    mod order;
+
+    pub use self::{ asset::*, funds::*, position::*, order::* };
+}
+pub mod indicators {
+    pub mod rsi;
+}
--- src/presenters/mod.rs
+++
@@ -1,4 +0,0 @@
-mod asset; pub use self::asset::*;
-mod funds; pub use self::funds::*;
-mod position; pub use self::position::*;
-mod order; pub use self::order::*;
